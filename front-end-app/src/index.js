4525a3bc1a4b18b706d8ed5eb067a27fcec4f42c
Tsvetomir Denchev <cvetomirdenchev@gmail.com>
2022-08-28 05:06:43 +0300

    add basic front - end

---
+++ front-end-app/src/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, createHttpLink, ApolloProvider, gql } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+
+const httpLink = createHttpLink({
+  uri: 'http://localhost:3000/graphql',
+});
+
+const authLink = setContext((_, { headers }) => {
+  return {
+    headers: {
+      ...headers,
+      authentication: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImNoYW5nZWRlbWFpbEBnbWFpbC5jb20iLCJpYXQiOjE2MjA5NDUyMjgsImV4cCI6MTYyMTAzMTYyOH0.IujCPXIBuqcYoHiJ4EZAwYPAUMEfp8L7J470sGDDpSU",
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      query {
+          borrow(_id:"609b8874f8f1f741248a68ab") {
+            _id
+            book {
+              _id
+              title
+              ISBN
+            }
+            reader {
+              _id
+              firstName
+              lastName
+            }
+            dateFrom
+            dateTo
+          }
+        }`,
+  })
+  .then((result) => console.log(result));
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
