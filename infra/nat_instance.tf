5f9aa23e05442877ffc5b1d69978f9a6769c5610
Tsvetomir Denchev <cvetomirdenchev@gmail.com>
2022-09-06 05:15:51 +0300

    add nat instance data

---
+++ infra/nat_instance.tf
@@ -0,0 +1,17 @@
+data "aws_ami" "al2_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "owner-alias"
+    values = ["amazon"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
