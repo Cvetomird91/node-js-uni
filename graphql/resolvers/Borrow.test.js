93003db04cc02f3a700786df0a91ffbe5a7a3dd3
Cvetomird91 <cvetomirdenchev@gmail.com>
2022-10-25 14:14:46 +0300

    update borrow test and models

--- graphql/resolvers/Borrow.test.js
+++ graphql/resolvers/Borrow.test.js
@@ -161,95 +161,95 @@ describe('Borrow resolver', () => {
 
    });
 
-//    it('attempt to borrow book from user with 0 status', async () => {
-//
-//        const reader = {
-//            _id: "609aee966fbcdd17db4d3d9a",
-//            firstName: "Tsvetomir",
-//            lastName: "Denchev",
-//            status: 0
-//        };
-//
-//        mockingoose(Borrow).toReturn([], 'find');
-//        mockingoose(Reader).toReturn(reader, 'findOne');
-//
-//        const result = await testServer.executeOperation({
-//          query: `mutation {
-//                    borrowBook(data: {
-//                      bookCopyId:"609ad74818789450a9b5ffb7",
-//                      readerId:"609aeed1a4a0d6192e5e8e1b",
-//                      dateFrom: "12-05-2021",
-//                      dateTo: "12-06-2021"
-//                    }) {
-//                      book {
-//                        _id
-//                        title
-//                        ISBN
-//                      }
-//                      reader {
-//                        _id
-//                        firstName
-//                        lastName
-//                      }
-//                      dateFrom
-//                      dateTo
-//                    }
-//                  }`
-//        });
-//
-//        expect(result.errors[0].message)
-//            .toEqual("The book copy is already borrowed! book copy 609ad74818789450a9b5ffb7");
-//
-//    });
-//
-//    it('attempt to borrow book from user with 0 status', async () => {
-//
-//        const reader = {
-//            _id: "609aee966fbcdd17db4d3d9a",
-//            firstName: "Tsvetomir",
-//            lastName: "Denchev",
-//            status: 1
-//        };
-//
-//        const borrow = [{
-//            _id: "609b8874f8f1f741248a68ab",
-//            bookCopy: "609ad6e718789450a9b5ffa5",
-//            readerId: reader,
-//            dateFrom: "2021-12-04T22:00:00.000Z",
-//            dateTo: "2021-12-05T22:00:00.000Z",
-//            status: 1
-//        }];
-//
-//        mockingoose(Borrow).toReturn([], 'find');
-//        mockingoose(Reader).toReturn(reader, 'findOne');
-//
-//        const result = await testServer.executeOperation({
-//          query: `mutation {
-//                    borrowBook(data: {
-//                      bookCopyId:"609ad74818789450a9b5ffb7",
-//                      readerId:"609aeed1a4a0d6192e5e8e1b",
-//                      dateFrom: "12-05-2021",
-//                      dateTo: "12-06-2021"
-//                    }) {
-//                      book {
-//                        _id
-//                        title
-//                        ISBN
-//                      }
-//                      reader {
-//                        _id
-//                        firstName
-//                        lastName
-//                      }
-//                      dateFrom
-//                      dateTo
-//                    }
-//                  }`
-//        });
-//
-//        expect(result.errors[0].message)
-//            .toEqual("The book copy is already borrowed! book copy 609ad74818789450a9b5ffb7");
-//
-//    });
+   it('attempt to borrow book from user with 0 status', async () => {
+
+       const reader = {
+           _id: "609aee966fbcdd17db4d3d9a",
+           firstName: "Tsvetomir",
+           lastName: "Denchev",
+           status: 0
+       };
+
+       mockingoose(Borrow).toReturn([], 'find');
+       mockingoose(Reader).toReturn(reader, 'findOne');
+
+       const result = await testServer.executeOperation({
+         query: `mutation {
+                   borrowBook(data: {
+                     bookCopyId:"609ad74818789450a9b5ffb7",
+                     readerId:"609aeed1a4a0d6192e5e8e1b",
+                     dateFrom: "12-05-2021",
+                     dateTo: "12-06-2021"
+                   }) {
+                     book {
+                       _id
+                       title
+                       ISBN
+                     }
+                     reader {
+                       _id
+                       firstName
+                       lastName
+                     }
+                     dateFrom
+                     dateTo
+                   }
+                 }`
+       });
+
+       expect(result.errors[0].message)
+           .toEqual("The reader is not active! reader id: 609aeed1a4a0d6192e5e8e1b");
+
+   });
+
+   it('attempt to borrow book from user with 0 status', async () => {
+
+       const reader = {
+           _id: "609aee966fbcdd17db4d3d9a",
+           firstName: "Tsvetomir",
+           lastName: "Denchev",
+           status: 1
+       };
+
+       const borrow = {
+           _id: "609b8874f8f1f741248a68ab",
+           bookCopy: "609ad6e718789450a9b5ffa5",
+           readerId: reader,
+           dateFrom: "2021-12-04T22:00:00.000Z",
+           dateTo: "2021-12-05T22:00:00.000Z",
+           status: 1
+       };
+
+       mockingoose(Borrow).toReturn(borrow, 'find');
+       mockingoose(Reader).toReturn(reader, 'findOne');
+
+       const result = await testServer.executeOperation({
+         query: `mutation {
+                   borrowBook(data: {
+                     bookCopyId:"609ad74818789450a9b5ffb7",
+                     readerId:"609aeed1a4a0d6192e5e8e1b",
+                     dateFrom: "12-05-2021",
+                     dateTo: "12-06-2021"
+                   }) {
+                     book {
+                       _id
+                       title
+                       ISBN
+                     }
+                     reader {
+                       _id
+                       firstName
+                       lastName
+                     }
+                     dateFrom
+                     dateTo
+                   }
+                 }`
+       });
+
+       expect(result.errors[0].message)
+           .toEqual("The book copy is already borrowed! book copy 609ad74818789450a9b5ffb7");
+
+   });
 
 });
