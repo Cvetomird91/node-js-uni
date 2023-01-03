cc1d702c0115382a0d1ccc2ab000b7dc99c3c3ef
Cvetomir Denchev <cvetomirdenchev@gmail.com>
2023-01-03 13:23:32 +0200

    book reducer tests

--- front-end-app/src/__tests__/state/books/BookReducer.test.ts
+++ front-end-app/src/__tests__/state/books/BookReducer.test.ts
@@ -1,7 +1,17 @@
-import { render } from '@testing-library/react';
 import { bookReducer, initialBookState } from '../../../../src/state/books/BookReducer';
 import { MOCK_BOOKS } from '../../__mocks__/MockBooks';
-import { LOAD_BOOKS_REQUEST, UPDATE_BOOK_SUCCESS } from '../../../../src/state/books/BookStateTypes';
+import { LOAD_BOOKS_SUCCESS,
+    LOAD_BOOKS_FAILURE,
+    LOAD_BOOKS_REQUEST,
+    UPDATE_BOOK_REQUEST,
+    UPDATE_BOOK_SUCCESS,
+    UPDATE_BOOK_FAILURE,
+    ADD_COPY_REQUEST,
+    ADD_COPY_SUCCESS,
+    ADD_COPY_FAILURE,
+    ADD_TITLE_REQUEST,
+    ADD_TITLE_SUCCESS,
+    ADD_TITLE_FAILURE } from '../../../../src/state/books/BookStateTypes';
 import Book from '../../../types/Book';
 
 describe('book reducer', () => {
@@ -39,6 +49,51 @@ describe('book reducer', () => {
     });
 
     test('should return all books', () => {
+        const books = MOCK_BOOKS;
+        const currentState = { ...initialBookState };
+        const loadedState = {
+            loading: false,
+            books,
+            error: ''
+        };
 
+        expect(
+            bookReducer(currentState, {
+                type: LOAD_BOOKS_SUCCESS,
+                payload: { books }
+            })
+        ).toEqual(loadedState);
     });
+
+    test('load books failure', () => {
+        const currentState = { ...initialBookState };
+        const loadedState = {
+            loading: false,
+            error: 'load books failure',
+            books: []
+        };
+
+        expect(
+            bookReducer(currentState, {
+                type: LOAD_BOOKS_FAILURE,
+                payload: { message: 'load books failure' }
+            })
+        ).toEqual(loadedState);
+
+    })
+
+    test('update book request', () => {
+        const currentState = { ...initialBookState };
+        const loadingState = {
+            ...initialBookState,
+            loading: false
+        };
+
+        expect(
+            bookReducer(currentState, {
+                type: UPDATE_BOOK_REQUEST
+            })
+        ).toEqual(loadingState)
+
+    })
 });
