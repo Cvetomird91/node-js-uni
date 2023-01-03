b2601c2bfd6391841d7994c4c37d9dca0901763d
Tsvetomir Denchev <cvetomirdenchev@gmail.com>
2023-01-03 23:35:25 +0200

    start reader reducer suite

---
+++ front-end-app/src/__tests__/__mocks__/MockReaders.ts
@@ -0,0 +1,7 @@
+import { Reader } from '../../types/Reader';
+
+export const MOCK_READERS = [
+    new Reader({fristName: "Ivan" , lastName: "Ivanov", status: 1}),
+    new Reader({fristName: "Dimitar" , lastName: "Dimitrov", status: 1}),
+    new Reader({fristName: "Georgi" , lastName: "Georgiev", status: 1}),
+];
---
+++ front-end-app/src/__tests__/state/readers/ReaderReducer.test.ts
@@ -0,0 +1,7 @@
+import {readerReducer, initialReaderState} from '../../../../src/state/readers/ReaderReducer';
+import {MOCK_READERS} from '../../__mocks__/MockReaders';
+import {
+    LOAD_READERS_REQUEST,
+    LOAD_READERS_SUCCESS,
+    LOAD_READERS_FAILURE
+} from '../../../../src/state/readers/ReaderStateTypes';
